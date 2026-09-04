e704882bc0a4f86df58406707da912d7caafed01
Ahmed-20021998 <158044760+Ahmed-20021998@users.noreply.github.com>
2026-09-05 01:31:24 +0300

    Add Vercel app URL to CORS middleware

--- main.py
+++ main.py
@@ -626,6 +626,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://next-lang-graph-ve46-ten.vercel.app/",
     ],
     allow_credentials=False,
     allow_methods=["*"],
